754ac055d4fa20e551532955812c81bfcb76557d
Yasin Ünan <unanyasin@gmail.com>
2023-09-28 11:21:25 +0300

    Initial commit

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,26 @@
+﻿
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal
+    {
+        //CRUD
+        //Type Name();
+
+        List<Category> List();
+
+        void Insert(Category P);
+
+        void Update(Category P);
+
+        void Delete(Category P);
+
+
+    }
+}
